54be8722aa5148a4559d039eae8514aeac4740c8
D.Solicen <DenisSolicen@yandex.ru>
2024-04-23 13:11:38 +0300

    Remove async and small refactoring
    I've just remove obsolete code and do small refactoring in methods. And update method for get RPA magic version.
    
    In a good way, all this had to be spread out into separate files. But I'm too lazy.

--- src/Archive.cs
+++ src/Archive.cs
@@ -1,4 +1,5 @@
 ﻿using System;using System.Collections.Generic;using System.IO;using System.IO.Compression;
+using System.Linq;
 using System.Text;using System.Text.RegularExpressions;using System.Threading.Tasks;
 using zlib; // zlib.net 
 
@@ -8,58 +9,25 @@ namespace Solicen.RenPy
 {
     class Archive
     {
-        static string RPA2_MAGIC = "RPA-2.0"; static string RPA3_MAGIC = "RPA-3.0"; static string RPA3_2MAGIC = "RPA-3.2";
         static string[] files; static string[] indexes; static double version; public static int Offset; public static string HeaderText;
+        static Dictionary<string, double> RPA_MAGIC = new Dictionary<string, double>()
+        {
+            {"RPA-2.0", 2},{"RPA-3.0", 3},{"RPA-3.2", 3.2},{"RPA-4.0", 4}
+
+        };
 
         //Получает версию RPA из файла
-        static double GetVersion(string path)
+        public static double GetVersion(string path)
         {
             string s = File.ReadAllLines(path)[0];
             string magic = s;
 
             Console.WriteLine("magic: " + s);
-            return 3;
+            var _magic = RPA_MAGIC.FirstOrDefault(x => magic.StartsWith(x.Key)).Value;
 
-            if (magic.StartsWith(RPA3_2MAGIC))
-                return 3.2;
-            else if (magic.StartsWith(RPA3_MAGIC))
-                return 3;
-            else if (magic.StartsWith(RPA2_MAGIC))
-                return 2;
-            else if (path.EndsWith(".rpi")) 
-                return 1;
-
-            return 0;
+            if (path.EndsWith(".rpi")) return 1;
+            else return _magic;
         }
-
-        //Находит все строки с .rpy 
-        static string[] RegexMatchesOfRPY(string f)
-        {
-            var r = Regex.Matches(f, "[A-z.-]*[.rpy]");
-            List<string> list = new List<string>();
-            foreach (var match in r)
-            {
-                list.Add(match.ToString());
-            }
-            return list.ToArray();
-        }
-
-        //Технически нужно только для не зашифрованных файлов .rpy внутри RPA
-        //У них можно определить их имя по внутрениму контексту
-        static string GetStringContainsOfRPY(string[] list)
-        {
-            var temp = "";
-            foreach (var l in list)
-            {
-                if (l.Contains(".rpy"))
-                {                 
-                    temp = l;
-                    break;
-                }
-            }
-            return temp;
-        }
-
 
         public static void CreateRPA(string outputPath, string InputDirectory)
         {
@@ -69,12 +37,12 @@ namespace Solicen.RenPy
             var files = Directory.GetFiles(InputDirectory, "*", SearchOption.AllDirectories);
 
             //2 Изменяем саму строку заголовка Zlib
-            var tZlib = CreateZlibHeader("", l);
+            var tZlib = Solicen.EX.Zlib.CreateZlibHeader("", l);
             //3. Изменить оффсет для будущего извлечения
-            var tOffset = ModifyZlibOffset(tZlib.Length, files, "");
+            var tOffset = Solicen.EX.Zlib.ModifyZlibOffset(tZlib.Length, files, "");
 
             //4 Компресируем новую строку заголовка Zlib
-            var header = CompressString(tZlib);
+            var header = Solicen.Compress.CompressString(tZlib);
 
             string newFile = "RPA-3.0 0x0000000 42424242";
             var index = 0; var lenghtFile = newFile.Length + 7;
@@ -89,7 +57,7 @@ namespace Solicen.RenPy
             }
 
             //6 Добавляем измененную строку
-            newFile = ReplaceZlibHeader(newFile, "", header);
+            newFile = Solicen.EX.Zlib.ReplaceZlibHeader(newFile, "", header);
 
             //7 Меняем оффсет на длину всех строк файла
             tOffset = lenghtFile;
@@ -122,16 +90,16 @@ namespace Solicen.RenPy
             ExtractZlibHeader(PATHtoFile);
 
             //3. Изменить оффсет для будущего извлечения
-            var tOffset = ModifyZlibOffset(Offset, files, HeaderText);
+            var tOffset = Solicen.EX.Zlib.ModifyZlibOffset(Offset, files, HeaderText);
 
             //4 Изменяем саму строку заголовка Zlib
-            var tZlib = CreateZlibHeader(HeaderText, l);
+            var tZlib = Solicen.EX.Zlib.CreateZlibHeader(HeaderText, l);
 
             //5 Компресируем новую строку заголовка Zlib
-            var header = CompressString(tZlib);
+            var header = Solicen.Compress.CompressString(tZlib);
 
             //6 Удаляем строку прочь из файла
-            newFile = ReplaceZlibHeader(newFile, HeaderText, " ");
+            newFile = Solicen.EX.Zlib.ReplaceZlibHeader(newFile, HeaderText, " ");
 
             var index = 0; var lenghtFile=newFile.Length+1;
             foreach (var file in files)
@@ -145,7 +113,7 @@ namespace Solicen.RenPy
             }
              
             //6 Добавляем измененную строку
-            newFile = ReplaceZlibHeader(newFile, "", header);
+            newFile = Solicen.EX.Zlib.ReplaceZlibHeader(newFile, "", header);
 
             //7 Меняем оффсет на длину всех строк файла
             tOffset = lenghtFile;
@@ -195,18 +163,8 @@ namespace Solicen.RenPy
 
         public static string[] ListFromDirectory(string directory)
         {
-            var list = new List<string>();
-            var data = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
-            if (data != null)
-            {
-                foreach (var d in data)
-                {
-                    Console.WriteLine(d.Replace(directory + "\\", ""));
-                    list.Add(d.Replace(directory + "\\", ""));
-                }
-            }
-            return list.ToArray();
-
+            return Directory.GetFiles(directory, "*", SearchOption.AllDirectories)
+                .Select(x => x.Replace($"{directory}\\", "")).ToArray();
         }
 
         //Создает поток из строки
@@ -220,20 +178,27 @@ namespace Solicen.RenPy
             return stream;
         }
 
+        public static string[] GetFilesList(string path)
+        {
+            if (!File.Exists(path)) return null;
+
+            Encoding enCode = Encoding.GetEncoding(1252);
+            string[] files = Regex.Split(File.ReadAllText(path, enCode), "Made with Ren'Py.", RegexOptions.Multiline);
+            ExtractZlibHeader(path); //Получаем индексы и заголовок файла
+            return files.Select(x => x.Split('/')[x.Split('/').Length - 1]).ToArray();
+        }
+
         static int index = 0;
-        public static async void ExtractArchive(string path, string output)
+        public static void ExtractArchive(string path, string output)
         {
             //Стандартная кодировка Windows 1251
             Encoding enCode = Encoding.GetEncoding(1252);
             string[] files = Regex.Split(File.ReadAllText(path, enCode), "Made with Ren'Py.", RegexOptions.Multiline);
             Console.WriteLine("Найдено файлов : " + (files.Length - 1));
+        
+            ExtractZlibHeader(path); //Получаем индексы и заголовок файла
 
-            //Получаем индексы и заголовок файла
-            ExtractZlibHeader(path);
-
-            var indexFile = 0;
-            var fileName = "";
-            var directory = output;
+            var indexFile = 0; string fileName = "", directory = output;
             var directoryName = directory.Split('/')[directory.Split('/').Length - 1];
             var filePath = "\\m\\";
 
@@ -242,17 +207,12 @@ namespace Solicen.RenPy
             {
                 //Если первая строка содержит RPA-X-X то пропустить данный 'файл'
                 if (f.StartsWith("RPA")) continue;
-
-                await Task.Delay(1);
                 fileName = ""; 
-
 
                 //Следующие два метода проверяют есть ли в файле RPA осмысленный текст от файл RPY
                 //То есть запакован ли в RPA файл обычный RPY файл без обфускации
                 //RegexMatchesOfRPY(f);
                 //if (f.Contains(".rpy")) fileName = GetStringContainsOfRPY(RegexMatchesOfRPY(f));
-
-
 
                 if (indexes.Length != 0)
                 {
@@ -261,8 +221,6 @@ namespace Solicen.RenPy
                     //Получаем путь до файла убирая имя файла
                     filePath = indexes[indexFile].Replace(fileName, "");
                 }
-
-
 
                 //Если имя файла не найдено.
                 if (fileName == "")
@@ -361,53 +319,12 @@ namespace Solicen.RenPy
             return directories.ToArray();
         }
 
-        static string[] GetAllFileName(string textfile)
-        {
-            Encoding enCode = Encoding.GetEncoding(1252);
-            var files = Regex.Split(File.ReadAllText(textfile, enCode), "Made with Ren'Py.", RegexOptions.Multiline);
-            List<string> fileNames = new List<string>();
-
-            int indexOfLenght = 2;
-            foreach (var file in files)
-            {
-                if (file.Length < 150) continue;
-                var f = file.Substring(0, 150);
-
-                //if (f.StartsWith("#") || f.StartsWith("RPA")) continue;
-                for (int i = 1; i < indexOfLenght; i++)
-                {
-                    if (f.Split('=').Length > i)
-                    {
-                        builder.AppendLine(f + "\n");
-                        Console.WriteLine("Имя файла : " + i + " : " + f.Split('=')[i]);
-                        fileNames.Add(f.Split('=')[i]);
-                    }
-                    else
-                    {
-                        builder.AppendLine(f + "\n\n");
-                        Console.WriteLine("Имя файла : 0 : " + f.Split('=')[0]);
-                        fileNames.Add(f.Split('=')[0]);
-                    }
-
-                }
-
-            }
-            builder.AppendLine("================================================");
-            builder.AppendLine("======" + " FilesCount : " + fileNames.Count + "======");
-            builder.AppendLine("=================================================");
-            Console.WriteLine("Найдено файлов : " + fileNames.Count);
-
-            //CreateFilesLog();
-            return fileNames.ToArray();
-        }
-
         static void ExtractZlibHeader(string path)
         {
-            var key = 0; version = 3;
-            indexes = null;
+            var key = 0; version = 3; indexes = null;
 
             var code = EncodingType.GetType(path);
-            Console.WriteLine("\nФайл         : " + path.Split('\\')[path.Split('\\').Length-1]);
+            Console.WriteLine("\nФайл         : " + Path.GetFileName(path));
             Console.WriteLine("Кодировка    : " + code.HeaderName);
 
             FileStream fileStream = new FileStream(path, FileMode.Open);
@@ -416,7 +333,7 @@ namespace Solicen.RenPy
             var fs = br.BaseStream; 
 
             fs.Seek(0, SeekOrigin.Current);
-            if (version == 2 || version == 3 || version == 3.2)
+            if (version == 2 || version == 3 || version == 3.2 || version == 4)
             {          
                 var metaData = reader.ReadLine();
                 var vals = metaData.Split(' ');
@@ -432,6 +349,7 @@ namespace Solicen.RenPy
                     key = 0;
                     key ^= Convert.ToInt32(vals[3], 16);
                 }
+
                 Console.WriteLine("RPA Offset   : " + offset);
                 Console.WriteLine("RPA EndKey   : " + key);
                 Console.WriteLine("Длина потока : " + reader.BaseStream.Length);
@@ -440,7 +358,7 @@ namespace Solicen.RenPy
                 byte[] bytesOfFile = ReadAllBytesOfStream(br.BaseStream, offset, (int)br.BaseStream.Length);
 
                 List<string> tempIndexes = new List<string>();
-                string final = DeDecompressToString(bytesOfFile);
+                string final = Solicen.Compress.DeDecompressToString(bytesOfFile);
                 Console.WriteLine("Финал : " + final + "\n");
                 Regex regex = new Regex(@"(?<=\x00\x00\x00).*?(?=\])", RegexOptions.IgnoreCase);
                 MatchCollection matches = regex.Matches(final);
@@ -449,12 +367,11 @@ namespace Solicen.RenPy
                 {
                     foreach (Match match in matches)
                     { 
-                        string s1 = match.ToString();
+                        string s1 = match.ToString(); 
                         Regex r1 = new Regex(@"q$|q.$");
                         string s = r1.Replace(s1, "");
 
-                        tempIndexes.Add(s);
-                        Console.WriteLine(s);
+                        tempIndexes.Add(s); Console.WriteLine(s);
                     }
                     
                 }
@@ -462,14 +379,33 @@ namespace Solicen.RenPy
                 Console.WriteLine("Количество совпадений : " + tempIndexes.Count);
                 tempIndexes.Sort(StringComparer.OrdinalIgnoreCase);
                 indexes = tempIndexes.ToArray();
-                fs.Close(); br.Close();
-                fileStream.Close();
-                Offset = offset;
-                HeaderText = final;
+                fs.Close(); br.Close(); fileStream.Close();
+
+                Offset = offset; HeaderText = final;
             }
         } 
 
+        static string ConvertToHex(string input)
+        {
+            var dataByte = Encoding.Default.GetBytes(input);
+            string HEX = BitConverter.ToString(dataByte);
+            HEX = HEX.Replace("-", "");
+            return HEX;
+        }
 
+        static StringBuilder builder = new StringBuilder();
+        static void CreateFilesLog()
+        {
+            File.WriteAllText(Environment.CurrentDirectory + "\\extractor.log", builder.ToString(), Encoding.GetEncoding(1252));
+            Environment.Exit(0);
+        }
+    }
+}
+
+namespace Solicen
+{
+    class Compress
+    {
         public static string CompressString(string s)
         {
             byte[] buffer = Encoding.Default.GetBytes(s);
@@ -488,38 +424,48 @@ namespace Solicen.RenPy
         //Декомпресирует строку Zlib
         public static string DeDecompressToString(byte[] buffer)
         {
-
             try
             {
                 MemoryStream memOutput = new MemoryStream();
                 ZOutputStream zipOut = new ZOutputStream(memOutput);
-
-                zipOut.Write(buffer, 0, buffer.Length);
-                zipOut.finish();
+                zipOut.Write(buffer, 0, buffer.Length); zipOut.finish();
 
                 memOutput.Seek(0, SeekOrigin.Begin);
                 byte[] result = memOutput.ToArray();
-
-                var str = Encoding.Default.GetString(result);
-                return str;
+                var str = Encoding.Default.GetString(result); return str;
             }
             catch
             {
-                string s = Encoding.Default.GetString(buffer);
-                return s;
+                string s = Encoding.Default.GetString(buffer); return s;
             }
-            return "";
         }
-        
+
+    }
+}
+
+namespace Solicen.EX
+{
+    class Zlib
+    {
         public static string CreateZlibHeader(string zlibHeader, string[] newFilesPATH)
         {
             var t = zlibHeader;
             foreach (var file in newFilesPATH)
-            {             
+            {
                 Console.WriteLine("Добавляю файл :" + file + " в заголовок");
                 t += "\x00\x00\x00" + file + "]";
             }
             return t;
+        }
+
+        public static int ModifyZlibOffset(int zlibOffset, string[] newFiles, string oldHeader)
+        {
+            var lenght = zlibOffset;
+            foreach (var file in newFiles)
+            {
+                lenght += file.Length;
+            }
+            return lenght - oldHeader.Length;
         }
 
         public static string ReplaceZlibHeader(string text, string rawZlibHeader, string newZlibHeader)
@@ -532,32 +478,7 @@ namespace Solicen.RenPy
             {
                 return text + "\n" + newZlibHeader;
             }
-            return "";
-        }
-
-        public static int ModifyZlibOffset(int zlibOffset, string[] newFiles, string oldHeader)
-        {
-            var lenght = zlibOffset;
-            foreach (var file in newFiles)
-            {
-                lenght += file.Length;
-            }
-            return lenght-oldHeader.Length;
-        }
-
-        static string ConvertToHex(string input)
-        {
-            var dataByte = Encoding.Default.GetBytes(input);
-            string HEX = BitConverter.ToString(dataByte);
-            HEX = HEX.Replace("-", "");
-            return HEX;
-        }
-
-        static StringBuilder builder = new StringBuilder();
-        static void CreateFilesLog()
-        {
-            File.WriteAllText(Environment.CurrentDirectory + "\\extractor.log", builder.ToString(), Encoding.GetEncoding(1252));
-            Environment.Exit(0);
         }
     }
 }
+
